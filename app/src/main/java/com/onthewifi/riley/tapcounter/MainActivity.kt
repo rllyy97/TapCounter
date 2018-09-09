352c8809f8edcc2d52e9117cfb3f36ada2bb9b1c
rllyy97 <rllyy97@gmail.com>
2018-09-09 15:53:31 -0500

    Added functionality

--- app/src/main/java/com/onthewifi/riley/tapcounter/MainActivity.kt
+++ app/src/main/java/com/onthewifi/riley/tapcounter/MainActivity.kt
@@ -2,11 +2,31 @@ package com.onthewifi.riley.tapcounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count = 0
+        val countButton: Button = findViewById(R.id.countButton)
+        val clearButton: FloatingActionButton = findViewById(R.id.clearButton)
+
+        countButton.text = 0.toString()
+
+        countButton.setOnClickListener {
+            count++
+            countButton.text = count.toString()
+        }
+
+        clearButton.setOnClickListener {
+            count = 0
+            countButton.text = count.toString()
+        }
     }
+
+
 }
